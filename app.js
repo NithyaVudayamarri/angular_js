7ff5c60fe7d30f26bd3dafb7e6a6180da3efa446
NithyaVudayamarri <nithya.vudayamarri43@gmail.com>
2019-05-22 18:27:00 -0500

    commited

---
+++ app.js
@@ -0,0 +1,6 @@
+angular.module("myApp", [])
+.controller("myFirstController", ['$scope', function ($scope) {
+    console.log("this is a form controller");
+$scope.Vudayamarri = "hi";
+$scope.hello = "hello world";
+}]);
